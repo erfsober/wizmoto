18fb637532604de91d1f98e477a86c574106037f
marzie <marziehrahmati1365@gmail.com>
2025-08-31 16:41:16 +0330

    add favicon

--- resources/views/wizmoto/partials/head.blade.php
+++ resources/views/wizmoto/partials/head.blade.php
@@ -9,8 +9,8 @@
     <link href="{{ asset('wizmoto/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('wizmoto/css/jquery-ui.css') }}" rel="stylesheet">
 
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset("wizmoto/images/favicon.png")}}" type="image/x-icon">
+    <link rel="icon" href="{{asset("wizmoto/images/favicon.png")}}" type="image/x-icon">
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
