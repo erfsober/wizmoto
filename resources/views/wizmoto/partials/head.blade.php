563e2a7ae82f635ef3bf58fa1b5cf20f08d108e4
marzie <marziehrahmati1365@gmail.com>
2025-09-15 21:32:36 +0330

    fix

--- resources/views/wizmoto/partials/head.blade.php
+++ resources/views/wizmoto/partials/head.blade.php
@@ -11,6 +11,9 @@
 
     <link rel="shortcut icon" href="{{asset("wizmoto/images/favicon.png")}}" type="image/x-icon">
     <link rel="icon" href="{{asset("wizmoto/images/favicon.png")}}" type="image/x-icon">
+    <!-- CSRF Token for AJAX requests -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
