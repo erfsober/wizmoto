822f0a0a868cea4ebe31306d0594ea0071215b83
marzie <marziehrahmati1365@gmail.com>
2025-09-16 23:19:09 +0330

    change chat approach

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 window.axios = axios;
 
+window.axios.defaults.baseURL = "https://wizmoto.com";
+
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
