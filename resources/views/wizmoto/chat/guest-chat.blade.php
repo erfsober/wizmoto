3a304c15bc5eba4bca0e3c9b68bd3b2732dfcdef
marzie <marziehrahmati1365@gmail.com>
2025-09-16 15:04:54 +0330

    change chat approach

--- resources/views/wizmoto/chat/guest-chat.blade.php
+++ resources/views/wizmoto/chat/guest-chat.blade.php
@@ -318,12 +318,12 @@
     // Then initialize Echo
     window.Echo = new Echo({
         broadcaster: "pusher",
-        key: import.meta.env.VITE_PUSHER_APP_KEY,
-        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+        key: "{{ env('PUSHER_APP_KEY') }}", 
+        cluster: "{{ env('PUSHER_APP_CLUSTER') }}",
         forceTLS: true,
-        wsHost: import.meta.env.VITE_PUSHER_HOST,
-        wsPort: import.meta.env.VITE_PUSHER_PORT,
-        wssPort: import.meta.env.VITE_PUSHER_PORT,
+        wsHost: "{{ env('PUSHER_HOST') }}",
+        wsPort: "{{ env('PUSHER_PORT') }}",
+        wssPort: "{{ env('PUSHER_PORT') }}",
         enabledTransports: ["ws", "wss"],
         authEndpoint: "/broadcasting/auth",
         auth: {
