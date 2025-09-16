2c2b9603bc52627e7dc991dcc5d36607eed1fbb9
marzie <marziehrahmati1365@gmail.com>
2025-09-16 21:22:13 +0330

    change chat approach

--- app/Http/Controllers/Wizmoto/Provider/Auth/ProviderController.php
+++ app/Http/Controllers/Wizmoto/Provider/Auth/ProviderController.php
@@ -7,38 +7,42 @@ use App\Models\Provider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
 
-class ProviderController extends Controller {
-    public function emailVerify ( Request $request , $id , $hash ) {
+class ProviderController extends Controller
+{
+    public function emailVerify(Request $request, $id, $hash)
+    {
         $provider = Provider::query()
-                            ->findOrFail($id);
-        if ( !hash_equals((string)$hash , sha1($provider->getEmailForVerification())) ) {
-            abort(403 , 'Invalid verification link.');
+            ->findOrFail($id);
+        if (!hash_equals((string)$hash, sha1($provider->getEmailForVerification()))) {
+            abort(403, 'Invalid verification link.');
         }
-        if ( $provider->hasVerifiedEmail() ) {
+        if ($provider->hasVerifiedEmail()) {
             return redirect()
                 ->route('home')
-                ->with('status' , 'Email already verified.');
+                ->with('status', 'Email already verified.');
         }
         $provider->markEmailAsVerified();
         event(new Verified($provider));
 
         return redirect()
             ->route('home')
-            ->with('status' , 'Email verified successfully!');
+            ->with('status', 'Email verified successfully!');
     }
 
-    public function show ( $id ) {
-        $provider = Provider::query()->with(['reviews'])
-                            ->where('id' , $id)
-                            ->firstOr(function () {
-                                return redirect()
-                                    ->back()
-                                    ->with('status' , 'This  provider doesnt exist!');
-                            });
-        $advertisements = $provider->advertisements()
-                                   ->latest()
-                                   ->take(10)->get();
+    public function show($id)
+    {
+        $provider = Provider::find($id);
 
-        return view('wizmoto.provider.show' , compact('provider' , 'advertisements'));
+        if (!$provider) {
+            return redirect()
+                ->back()
+                ->with('status', 'This provider doesnt exist!');
+        }
+
+        $advertisements = $provider->advertisements()
+            ->latest()
+            ->take(10)->get();
+
+        return view('wizmoto.provider.show', compact('provider', 'advertisements'));
     }
 }
