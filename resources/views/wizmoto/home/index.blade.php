6d4c82ba17435ea2bd7d8d91d76fc80aed48e261
marzie <marziehrahmati1365@gmail.com>
2025-10-15 23:06:13 +0330

    add link for home card

--- resources/views/wizmoto/home/index.blade.php
+++ resources/views/wizmoto/home/index.blade.php
@@ -483,7 +483,7 @@
                         <h3 class="title">Are You Looking
                             <br>For a Motorcycle ?</h3>
                         <div class="text">We are committed to providing our customers with exceptional service.</div>
-                        <a href="pricing.html" class="read-more">Get Started
+                        <a href="{{ route('inventory.list') }}" class="read-more">Get Started
                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewbox="0 0 14 14" fill="none">
                                 <g clip-path="url(#clip0_601_692)">
                                     <path d="M13.6109 0H5.05533C4.84037 0 4.66643 0.173943 4.66643 0.388901C4.66643 0.603859 4.84037 0.777802 5.05533 0.777802H12.6721L0.113697 13.3362C-0.0382246 13.4881 -0.0382246 13.7342 0.113697 13.8861C0.18964 13.962 0.289171 14 0.388666 14C0.488161 14 0.587656 13.962 0.663635 13.8861L13.222 1.3277V8.94447C13.222 9.15943 13.3959 9.33337 13.6109 9.33337C13.8259 9.33337 13.9998 9.15943 13.9998 8.94447V0.388901C13.9998 0.173943 13.8258 0 13.6109 0Z" fill="white"></path>
@@ -513,7 +513,7 @@
                         <h3 class="title">Do You Want to
                             <br>Sell a Motorcycle ?</h3>
                         <div class="text">We are committed to providing our customers with exceptional service.</div>
-                        <a href="pricing.html" class="read-more">Get Started
+                        <a href="{{ route('dashboard.create-advertisement') }}" class="read-more">Get Started
                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewbox="0 0 14 14" fill="none">
                                 <g clip-path="url(#clip0_601_692)">
                                     <path d="M13.6109 0H5.05533C4.84037 0 4.66643 0.173943 4.66643 0.388901C4.66643 0.603859 4.84037 0.777802 5.05533 0.777802H12.6721L0.113697 13.3362C-0.0382246 13.4881 -0.0382246 13.7342 0.113697 13.8861C0.18964 13.962 0.289171 14 0.388666 14C0.488161 14 0.587656 13.962 0.663635 13.8861L13.222 1.3277V8.94447C13.222 9.15943 13.3959 9.33337 13.6109 9.33337C13.8259 9.33337 13.9998 9.15943 13.9998 8.94447V0.388901C13.9998 0.173943 13.8258 0 13.6109 0Z" fill="white"></path>
